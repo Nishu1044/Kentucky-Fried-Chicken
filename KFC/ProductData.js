961abb8197e600a6ed3aac0b6946bfdb293bb9da
Nishu Kumari <123900586+Nishu1044@users.noreply.github.com>
2024-07-28 16:54:11 +0530

    Add files via upload

---
+++ KFC/ProductData.js
@@ -0,0 +1,413 @@
+
+let ProData = [
+   
+    {
+        title : "PERI PERI CHICKEN",
+        name : "5 Leg Pc & 2 Dips Bucket",
+        type : " Non veg Serves 2",
+        price : "₹519.05",
+        description : "Save Rs. 120 on 5 Peri Peri Leg Pieces, paired with 2 delicious dips (20gm each)",
+        image: "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002144.jpg?ver=41.8"
+        
+    },
+
+    {
+        title : "PERI PERI CHICKEN",
+        name : "Peri Peri 10 Pc Chicken Strips & 2 Dips",
+        type : " Non veg Serves 2",
+        price : "₹499.05",
+        description : "Save 24% on 10 Spicy Peri Peri chicken boneless strips with 2 Dips",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002319.jpg?ver=41.8"
+    },
+
+    {
+        title : "PERI PERI CHICKEN",
+        name : "Peri Peri 10 Leg Pc & 4 Dips",
+        type : " Non veg Serves 2",
+        price : "₹948.57",
+        description : "Save Rs. 334 on 10 Peri Peri Leg Pieces & 4 Dips (20 gm each)",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002141.jpg?ver=41.8"
+    },
+
+
+
+
+
+    {
+        title : "VALUE SNACKERS",
+        name : "Single Chicken Roll",
+        type : " Non veg Serves 2",
+        price : "₹119.05",
+        description : "Street style roll with single chicken strip, onions and two spicy sauce",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-K617.jpg?ver=41.8"
+    },
+
+
+    {
+        title : "VALUE SNACKERS",
+        name : "Chicken Longer Burger & 2 Strips Combo",
+        type : " Non veg Serves 2",
+        price : "₹219.05",
+        description : "Longgg burger with crunchy chicken, onions & a punchy sauce , served with 2 peri peri chicken strips",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002348.jpg?ver=41.8"
+    },
+
+    {
+        title : "VALUE SNACKERS",
+        name : "Chocolate Lava Cake",
+        type : " veg Serves 2",
+        price : "₹108.57",
+        description : "Soft Chocolate cake with a gooey center- perfect chocolaty end to every meal",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-K701.jpg?ver=41.8"
+    },
+
+
+
+
+
+
+
+
+    {
+        title : "CHICKEN ROLLS",
+        name : "Classic Chicken Roll & Pepsi Combo",
+        type : " Non veg Serves 1",
+        price : "₹176.19",
+        description : "Our new Classic Chicken Roll served with Pepsi 475ml",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002324.jpg?ver=41.8"
+    },
+
+    {
+        title : "CHICKEN ROLLS",
+        name : "Classic Chicken Roll Meal for 1",
+        type : " Non veg Serves 1",
+        price : "₹339.05",
+        description : "Deal combo of 1 Classic Chicken Roll, Large Fries, 2 Dips & Pepsi 475ml",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002070.jpg?ver=41.8"
+    },
+
+
+    {
+        title : "CHICKEN ROLLS",
+        name : "2 Double Chicken Roll",
+        type : " Non veg Serves 1",
+        price : "₹328.57",
+        description : "Double the fun with 2 Double Chicken Rolls",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002316.jpg?ver=41.8"
+    },
+
+
+    {
+        title : "CHICKEN ROLLS",
+        name : "Double Chicken Roll",
+        type : " Non veg Serves 1",
+        price : "₹168.57",
+        description : "KFC's take on the street style roll with double chicken strips, onions & spicy sauces",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-K657.jpg?ver=41.8"
+    },
+
+
+
+
+
+
+
+
+
+
+
+    {
+        title : "CHICKEN BUCKETS",
+        name : "The Allu Arjun Combo",
+        type : " Non veg Serves 1",
+        price : "₹448.57",
+        description : "Try Allus fav-1 Hot & Crispy Chicken,1 Smoky Red, Reg Popcorn, Spicy Fries & 1 Dip 20gm",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002228.jpg?ver=41.8"
+    },
+
+    {
+        title : "CHICKEN BUCKETS",
+        name : "Bucket for Two",
+        type : " Non veg Serves 1",
+        price : "₹599.05",
+        description : "Flat Rs. 115 off on combo of 2pc Hot & Crispy, 2 pc Smoky Red Chicken & Large Popcorn",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00000892.jpg?ver=41.8"
+    },
+
+    {
+        title : "CHICKEN BUCKETS",
+        name : "Stay Home Bucket",
+        type : " Non veg Serves 1",
+        price : "₹788.57",
+        description : "Save 21% on 4 Hot & Crispy, 4 Wings, 6 Peri Strips & 2 dips (20gm each)",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002169.jpg?ver=41.8"
+    },
+
+
+
+
+
+    
+    {
+        title : "BIRYANI BUCKETS",
+        name : "Classic Biryani Combo",
+        type : " Non veg Serves 1",
+        price : "₹740.00",
+        description : "Large Biryani rice with 2 Hot & Crispy, 2 Gravies, 4 Strips & 1 Pepsi 475ml",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002166.jpg?ver=41.8"
+    },
+
+    {
+        title : "BIRYANI BUCKETS",
+        name : "Classic Chicken Biryani Bucket (Large",
+        type : " Non veg Serves 1",
+        price : "₹475.24",
+        description : "Large portions of Biryani rice served with 2 pc Hot & Crispy Chicken & 2 Gravies",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-K626.jpg?ver=41.8"
+    },
+
+    {
+        title : "BIRYANI BUCKETS",
+        name : "Veg Biryani Bucket",
+        type : "Veg Serves 1",
+        price : "₹199.05",
+        description : "Hyderabadi style Biryani rice served with Veg Patty & Spicy Gravy",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-K622.jpg?ver=41.8"
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+    {
+        title : "BOX MEALS",
+        name : "All Chicken Box",
+        type : " Non veg",
+        price : "₹189.52",
+        description : "A Box with your favorites! Get 1 pc Hot & Crispy, 2 Hot Wings & 1 Chicken Strip at a deal price",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002167.jpg?ver=41.8"
+    },
+
+   
+    {
+        title : "BOX MEALS",
+        name : "Veg Biryani Box",
+        type : "veg",
+        price : "₹189.52",
+        description : "Biryani lovers unite : Get 1 Veg Biryani Bucket with gravy, 1 Veg Patty & 1 Pepsi 475ml",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00001038.jpg?ver=41.8"
+    },
+
+
+
+
+
+
+
+
+    {
+        title : "BURGERS",
+        name : "Family Feast",
+        type : " Non veg Serves 3",
+        price : "₹859.05",
+        description : "Family Deal of 3 chicken zingers, med popcorn, med fries & 1 Pepsi 475ml",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00000974.jpg?ver=41.8"
+    },
+
+    
+    {
+        title : "BURGERS",
+        name : "2 Veg Krisper Burgers",
+        type : "veg Serves 3",
+        price : "₹179.05",
+        description : "2 delicious veg value burgers - at a deal price",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00001022.jpg?ver=41.8"
+    },
+
+    {
+        title : "BURGERS",
+        name : "Classic Zinger Burger",
+        type : " Non veg Serves 3",
+        price : "₹199.05",
+        description : "Signature burger made with a crunchy chicken fillet, veggies & a delicious mayo sauce",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-K439.jpg?ver=41.8"
+    },
+
+
+    {
+        title : "BURGERS",
+        name : "2 Veg Krispers Meal",
+        type : "veg Serves 3",
+        price : "₹288.57",
+        description : "Family Deal of 3 chicken zingers, med popcorn, med fries & 1 Pepsi 475ml",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00001023.jpg?ver=41.8"
+    },
+  
+
+
+
+
+
+
+
+
+    {
+        title : "SNACKS",
+        name : "Chocolate Lava Cake",
+        type : "veg Serves 3",
+        price : "₹108.57",
+        description : "Soft Chocolate cake with a gooey center- perfect chocolaty end to every meal",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-K701.jpg?ver=41.8"
+    },
+
+
+    {
+        title : "SNACKS",
+        name : "Large Fries",
+        type : "veg Serves 3",
+        price : "₹119.05",
+        description : "Jazz up your meal with crispy large fries!",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-K138.jpg?ver=41.8"
+    },
+
+
+    {
+        title : "SNACKS",
+        name : "Peri Peri 3pc Chicken Strips",
+        type : "Non veg Serves 3",
+        price : "₹179.05",
+        description : "3 Spicy Peri Peri chicken strips",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002159.jpg?ver=41.8"
+    },
+
+    {
+        title : "SNACKS",
+        name : "2 pc Veg Patty",
+        type : "veg Serves 3",
+        price : "₹155.24",
+        description : "Delicious, crispy, veg add-on",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-K351.jpg?ver=41.8"
+    },
+
+
+
+    {
+        title : "BEVERAGES",
+        name : "Pepsi 475ml PET",
+        type : "veg Serves 3",
+        price : "₹55",
+        description : "Pepsi 475ml PET",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-K350.jpg?ver=41.8"
+    },
+
+    {
+        title : "BEVERAGES",
+        name : "7UP Can 300 ml",
+        type : "veg Serves 3",
+        price : "₹57.14",
+        description : "7UP Can 300 ml",
+        image : "https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-K487.jpg?ver=41.8"
+    },
+
+
+]
+
+
+let container = document.querySelector("#container");
+
+let inputbox = document.querySelector("div>input");
+let searchBtn = document.querySelector("#seaBtn");
+
+
+/local storage part/ 
+let LSData = localStorage.getItem("cart");
+if(LSData === null){
+    LSData = [];
+}else{
+    LSData = JSON.parse(LSData);
+}
+
+
+function ShowData(arr){
+    container.innerHTML=""
+     
+    arr.forEach(function(ele,i){
+
+        let separateDiv = document.createElement("div")
+        separateDiv.classList.add("product");
+        
+         let image = document.createElement("img")
+         image.src = ele.image
+        
+          let Title = document.createElement("h3")
+          Title.innerHTML = ele.title
+        
+          let Name = document.createElement("h4")
+          Name.innerHTML = ele.name
+        
+        
+          let Type = document.createElement("p")
+          Type.innerHTML = ele.type
+        
+        
+          let Price = document.createElement("h5")
+          Price.innerHTML = ele.price
+        
+          let Des = document.createElement("p")
+          Des.innerHTML = ele.description
+        
+          let btn = document.createElement("Button")
+          btn.innerText = "Add"
+
+          btn.addEventListener("click", function() {
+            AddToCart(ele); // Passing product info to AddToCart function
+        });
+         
+        
+          separateDiv.append(image,Title,Name,Type,Price,Des,btn)
+          container.append(separateDiv)
+         });
+}
+
+ShowData(ProData);
+
+
+ function searchData(){
+   let value = inputbox.value;
+  
+   let newarr = ProData.filter(function(ele,i){
+     return ele.title.toLowerCase() === value.toLowerCase() || ele.name.toLowerCase() === value.toLowerCase();
+    
+   })
+//    console.log(newarr);
+
+if(newarr.length>0){
+    ShowData(newarr)
+}else{
+    container.innerHTML = "<h2> Product is not found ..... <h2/>"
+}
+
+ }
+
+ searchBtn.addEventListener("click", searchData)
+
+//  function AddToCart(){
+
+//     LSData.push(ele);
+//     localStorage.setItem("cart",JSON.stringify(LSData))
+//  }
+
+
+function AddToCart(product){
+    LSData.push(product);
+    localStorage.setItem("cart", JSON.stringify(LSData));
+    alert("Item added to cart!");
+}
